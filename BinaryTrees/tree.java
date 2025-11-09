c471fd14d6c4efec7e4296a33948a4ec0390d1dd
sdan-07 <soumyadipdan7@gmail.com>
2025-11-09 17:09:25 +0530

    committed

--- BinaryTrees/tree.java
+++ BinaryTrees/tree.java
@@ -1,3 +1,7 @@
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
 class Node{
     int data;
     Node left;
@@ -78,22 +82,52 @@ public class tree{
         }
     }
 
+    //Level order traversal
+    public void levelOrder(){
+        Queue<Node> queue = new LinkedList<>();
+        List<List<Integer>> wraplst = new LinkedList<>();
+
+        if(root == null) System.out.print("Empty tree");
+
+        queue.offer(root);
+
+        while(!queue.isEmpty()){
+            List<Integer> sublst = new LinkedList<>();
+
+            int queueLen = queue.size();
+            for(int i=0; i<queueLen; i++){
+                if(queue.peek().left != null)  queue.offer(queue.peek().left);
+                if(queue.peek().right != null)  queue.offer(queue.peek().right);
+
+                sublst.add(queue.poll().data);
+            }
+            wraplst.add(sublst);
+        }
+
+        System.out.println(wraplst);
+
+    }
+
+
     public static void main(String[] args) {
         tree tree = new tree();
+
         tree.insert(21);
         tree.insert(40);
         tree.insert(33);
         tree.insert(12);
-
         tree.insert(97);
 
-        tree.Inorder();
+//        tree.Inorder();
+//        System.out.println();
+//
+//        tree.preorder();
+//        System.out.println();
+//
+//        tree.postorder();
         System.out.println();
 
-        tree.preorder();
-        System.out.println();
-
-        tree.postorder();
+        tree.levelOrder();
     }
 }
 
