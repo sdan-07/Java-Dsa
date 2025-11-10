99444c1faa1d1ce071e6c2057080fe1afcde2cf2
sdan-07 <soumyadipdan7@gmail.com>
2025-11-10 11:14:58 +0530

    committed

--- LinkedList/questions/reverseLL.java
+++ LinkedList/questions/reverseLL.java
@@ -1,4 +1,67 @@
 package questions;
 
-public class reverseLL {
+class listNode {
+    int data;
+    listNode next;
+
+    listNode(int data){ this.data=data; }
 }
+
+public class reverseLL {
+    listNode head=null, tail=null;
+
+    public void insert_end(int data){
+        listNode nn = new listNode(data);
+        if(head == null)
+            head=tail=nn;
+        else{
+            tail.next = nn;
+            tail = nn;
+        }
+    }
+
+    public void print(){
+        listNode t = head;
+        while(t!=null){
+            System.out.print(t.data+" -> ");
+            t=t.next;
+        }
+        System.out.print("null");
+    }
+
+    //reverse function
+    public listNode reverse(){
+        listNode prevptr = null, curr = head;
+
+        while(curr != null){
+            listNode nextptr = curr.next;
+            curr.next = prevptr;
+            prevptr = curr;
+            curr = nextptr;
+        }
+
+        return prevptr;
+    }
+
+
+    public static void main(String[] args) {
+        reverseLL ll = new reverseLL();
+
+        ll.insert_end(42);
+        ll.insert_end(78);
+        ll.insert_end(89);
+
+        System.out.println("Original list:");
+        ll.print();
+
+        System.out.println("\nAfter reverseing list:");
+        ll.head = ll.reverse();
+        ll.print();
+
+    }
+}
+
+
+
+
+
